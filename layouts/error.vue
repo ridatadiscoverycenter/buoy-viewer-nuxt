f625466ca1830f3056ef4057a2e1e05a2e5bedbe
fernandogelin <fernando@fernandogelin.com>
2020-10-27 16:35:04 -0400

    feat: 🎸 add error page, clean-up vuex actions

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Error Page. {{ error.statusCode }}</div>
+  <main class="error-container">
+    <div class="error-content">
+      <fa
+        :icon="['fad', 'life-ring']"
+        size="5x"
+        class="mr-2 life-ring has-text-danger"
+      />
+      <p>{{ error.message }}</p>
+      <p v-if="error.statusCode >= 500">
+        Make sure you selected a data range for which we have data.
+      </p>
+    </div>
+    <nuxt-link to="/">
+      <fa :icon="['fas', 'arrow-left']" class="mr-2" />
+      Back to home</nuxt-link
+    >
+  </main>
 </template>
 
 <script>
@@ -9,4 +25,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.error-content,
+.error-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.error-container {
+  padding: 3rem;
+  margin-top: 5rem;
+  height: 100%;
+}
+.error-content {
+  font-size: 3rem;
+}
+</style>
