e9fd73fe77e71e0e1b57bd166bf469fc18a14849
fernandogelin <fernando@fernandogelin.com>
2020-10-27 13:49:08 -0400

    fix: 🐛 fix base path for vm deployment

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Error Page. {{ error.statusCode }}</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error']
+};
+</script>
+
+<style lang="scss" scoped></style>
