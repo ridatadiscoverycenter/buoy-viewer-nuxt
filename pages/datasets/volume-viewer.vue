0e4e112a866ad6b042e58e4678512103b44ca290
Ellen Duong <shi.ellenduong@gmail.com>
2021-07-06 14:41:09 -0400

    feat: new page for volume viewer

---
+++ pages/datasets/volume-viewer.vue
@@ -0,0 +1,28 @@
+<template>
+  <Dashboard>
+    <template #main-nav>
+      <nuxt-link
+        class="plot-nav is-size-5 py-5"
+        :to="{
+          name: 'index',
+        }"
+        ><span>Home</span></nuxt-link
+      ></template
+    >
+    <template #main-header><h1 class="title">Volume Viewer</h1></template>
+    <template #main-section></template>
+  </Dashboard>
+</template>
+
+<script>
+import Dashboard from '@/components/base/BaseDashboard.vue';
+
+export default {
+  components: {
+    Dashboard,
+  },
+  layout: 'dashboard',
+};
+</script>
+
+<style scoped></style>
